66c50b45c64d72c89b01210fe9bcb13ee6eb4c91
Jacquie24 <jgcaption@yahoo.com>
2021-01-09 08:52:18 -0500

    Added additional license badge.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,6 +7,8 @@ function renderLicenseBadge(license) {
     return `[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)`;
   } else if (license === "GNU GPLv3") {
     return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  } else if (license === "BSD 3-Clause") {
+    return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
   } else if (license === "none") {
     return "";
   }
